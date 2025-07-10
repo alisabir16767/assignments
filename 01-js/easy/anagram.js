842b2b8a6d2c837498cbb5e824b1077220554b4e
Sabir Ali <alisabir167167@gmail.com>
2025-07-10 10:13:03 +0530

    first completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,13 +5,9 @@
 */
 
 function isAnagram(str1, str2) {
-  str1.sort();
-  str2.sort();
-  if (str1.length !== str2.length) return false;
-  for (let i = 0; i < str1.length; i++) {
-    if (str1[i] !== str2[i]) return false;
-  }
-  return true;
+  str1 = str1.toLowerCase().split("").sort().join("");
+  str2 = str2.toLowerCase().split("").sort().join("");
+  return str1 === str2;
 }
 
 module.exports = isAnagram;
